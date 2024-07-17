169f6c3833f3677b5a8e4ff170f0c27e2ab1fb8f
v1148656 <v1148656@gmail.com>
2024-07-17 16:17:49 +0300

    last commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,20 @@
-import { Outlet, useParams } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Navigation from './Navigation'
-import { useEffect } from 'react'
-import { selectUser } from '../redux/usersSlice';
-import { AppDispatch } from '../redux/store';
-import { useDispatch } from 'react-redux';
+// import { useEffect } from 'react'
+// import { selectUser } from '../redux/usersSlice';
+// import { AppDispatch } from '../redux/store';
+// import { useDispatch } from 'react-redux';
 
 // rafce
 const Layout = () => {
-  const { id } = useParams();
-  const dispatch: AppDispatch = useDispatch();
+  // const { id } = useParams();
+  // const dispatch: AppDispatch = useDispatch();
 
-  useEffect(() => {
-    if (id) {
-      dispatch(selectUser(+id))
-    }
-  }, [id])
+  // useEffect(() => {
+  //   if (id) {
+  //     dispatch(selectUser(+id))
+  //   }
+  // }, [id])
 
   return (
     <div>
